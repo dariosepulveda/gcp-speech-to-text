9635966fcdf878c55956d2c4f77b721fca5ea435
MacBook <macbook@MacBook-de-MacBook.local>
2019-11-06 01:32:24 -0300

    test3

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 var express = require('express');
 var app = express();
 var cors = require('cors');
@@ -9,10 +10,55 @@ app.use(cors({
 }));
 
 app.get('/',[], async (req, res)=>{
-res.json({ msg:'hola Felipe!' }); 
+
+
+
+const fs = require('fs');
+
+// Imports the Google Cloud client library
+const speech = require('@google-cloud/speech');
+
+// Creates a client
+const client = new speech.SpeechClient();
+
+/**
+ * TODO(developer): Uncomment the following lines before running the sample.
+ */
+const filename = './audio.raw';
+const encoding = 'LINEAR16';
+const sampleRateHertz = 16000;
+const languageCode = 'en-US';
+
+const request = {
+  config: {
+    encoding: encoding,
+    sampleRateHertz: sampleRateHertz,
+    languageCode: languageCode,
+  },
+  interimResults: false, // If you want interim results, set this to true
+};
+
+// Stream the audio to the Google Cloud Speech API
+const recognizeStream = client
+  .streamingRecognize(request)
+  .on('error', console.error)
+  .on('data', data => {
+    console.log(
+      `Transcription: ${data.results[0].alternatives[0].transcript}`
+    );
+
+res.json({ msg:data.results[0].alternatives[0].transcript }); 
+
+  });
+
+// Stream an audio file from disk to the Speech API, e.g. "./resources/audio.raw"
+fs.createReadStream(filename).pipe(recognizeStream);
+
+
+
+
 });
 
 app.listen(4600, function () {
   console.log('Listening on port 4600!');
 });
-
--- index1.js
+++ index1.js
@@ -1,37 +1,41 @@
-const fs = require('fs');
+async function main() {
+  // Imports the Google Cloud client library
+  const speech = require('@google-cloud/speech');
+  const fs = require('fs');
 
-// Imports the Google Cloud client library
-const speech = require('@google-cloud/speech');
+  // Creates a client
+  const client = new speech.SpeechClient();
 
-// Creates a client
-const client = new speech.SpeechClient();
+  // The name of the audio file to transcribe
+  const fileName = './audio.mp3';
 
-/**
- * TODO(developer): Uncomment the following lines before running the sample.
- */
-// const filename = 'Local path to audio file, e.g. /path/to/audio.raw';
- const encoding = 'LINEAR16';
- const sampleRateHertz = 16000;
- const languageCode = 'es-ES';
+  // Reads a local audio file and converts it to base64
+  //const file = fs.readFileSync(fileName);
+  const audioBytes = fs.readFileSync(fileName).toString('base64');
 
-const request = {
-  config: {
-    encoding: encoding,
-    sampleRateHertz: sampleRateHertz,
-    languageCode: languageCode,
-  },
-  interimResults: false, // If you want interim results, set this to true
-};
+  //console.log(audioBytes)
 
-// Stream the audio to the Google Cloud Speech API
-const recognizeStream = client
-  .streamingRecognize(request)
-  .on('error', console.error)
-  .on('data', data => {
-    console.log(
-      `Transcription: ${data.results[0].alternatives[0].transcript}`
-    );
-  });
+  // The audio file's encoding, sample rate in hertz, and BCP-47 language code
+  const audio = {
+    content: audioBytes,
+  };
+  const config = {
+    encoding: `LINEAR16`,
+    sampleRateHertz: 16000,
+    languageCode: `es-ES`,
+    //model: `phone_call`,
+  };
+  const request = {
+    audio: audio,
+    config: config,
+  };
 
-// Stream an audio file from disk to the Speech API, e.g. "./resources/audio.raw"
-fs.createReadStream(filename).pipe(recognizeStream);
+ const [response] = await client.recognize(request);
+  const transcription = response.results
+    .map(result => result.alternatives[0].transcript)
+    .join('\n');
+
+  console.log(response);
+  
+}
+main().catch(console.error);
